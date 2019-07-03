1221e41b60c039d3e33ae3615fa1e5c96f8cfc01
vasimlook <vasimlook@gmail.com>
2019-07-03 08:40:49 +0530

    map integration testing
    map integration testing

--- application/MVC/View/frontcommon/epaymentjs.php
+++ application/MVC/View/frontcommon/epaymentjs.php
@@ -122,70 +122,70 @@
             var vehicleno = '';
             var totaltax = 0;
             var noofpassenger = 0;
-            
-            if (taxtypeid != 0) {                
-                    if (commodityid == 0) {
-                        alertstr.push("Please select Commodity/Description.");
+
+            if (taxtypeid != 0) {
+                if (commodityid == 0) {
+                    alertstr.push("Please select Commodity/Description.");
+                    returnval = 1;
+                } else {
+                    if ($("#rateunit").length) {
+                        weight = $("#rateunit").val();
+                        if (weight === '' || weight === 0) {
+                            alertstr.push("Please enter weight.");
+                            returnval = 1;
+                        }
+                    }
+                }
+                if ($("#vehicleno").length) {
+                    vehicleno = $("#vehicleno").val();
+                    if (vehicleno === '' || vehicleno === 0) {
+                        alertstr.push("Please enter Vehicle Number.");
                         returnval = 1;
-                    } else {
-                        if ($("#rateunit").length) {
-                            weight = $("#rateunit").val();
-                            if (weight === '' || weight === 0) {
-                                alertstr.push("Please enter weight.");
-                                returnval = 1;
-                            }
-                        }
                     }
-                    if ($("#vehicleno").length) {
-                        vehicleno = $("#vehicleno").val();
-                        if (vehicleno === '' || vehicleno === 0) {
-                            alertstr.push("Please enter Vehicle Number.");
-                            returnval = 1;
-                        }
+                }
+                if ($("#mesuare").length) {
+                    mesuare = $("#mesuare").val();
+                }
+                if ($("#sourcelocation").length) {
+                    sourcelocation = $("#sourcelocation").val();
+                    if (sourcelocation === '') {
+                        alertstr.push("Please enter Source Location.");
+                        returnval = 1;
                     }
-                    if ($("#mesuare").length) {
-                        mesuare = $("#mesuare").val();
+                }
+                if ($("#destinationlocation").length) {
+                    destinationlocation = $("#destinationlocation").val();
+                    if (destinationlocation === '') {
+                        alertstr.push("Please enter Destination Location.");
+                        returnval = 1;
                     }
-                    if ($("#sourcelocation").length) {
-                        sourcelocation = $("#sourcelocation").val();
-                        if (sourcelocation === '') {
-                            alertstr.push("Please enter Source Location.");
-                            returnval = 1;
-                        }
+                }
+                if ($("#distance").length) {
+                    distance = $("#distance").val();
+                    if (distance === '' || distance === 0) {
+                        alertstr.push("Please enter Distance.");
+                        returnval = 1;
                     }
-                    if ($("#destinationlocation").length) {
-                        destinationlocation = $("#destinationlocation").val();
-                        if (destinationlocation === '') {
-                            alertstr.push("Please enter Destination Location.");
-                            returnval = 1;
-                        }
+                }
+                if ($("#noofpassenger").length) {
+                    noofpassenger = $("#noofpassenger").val();
+                    if (noofpassenger === '' || noofpassenger === 0) {
+                        alertstr.push("Please enter Number of Passenger.");
+                        returnval = 1;
                     }
-                    if ($("#distance").length) {
-                        distance = $("#distance").val();
-                        if (distance === '' || distance === 0) {
-                            alertstr.push("Please enter Distance.");
-                            returnval = 1;
-                        }
-                    }                    
-                    if ($("#noofpassenger").length) {
-                        noofpassenger = $("#noofpassenger").val();
-                        if (noofpassenger === '' || noofpassenger === 0) {
-                            alertstr.push("Please enter Number of Passenger.");
-                            returnval = 1;
-                        }
-                    }                    
-                    if ($("#totaltax").length) {
-                        totaltax = $("#totaltax").val();
+                }
+                if ($("#totaltax").length) {
+                    totaltax = $("#totaltax").val();
+                }
+                if (returnval == 1) {
+                    var text = "";
+                    var i;
+                    for (i = 0; i < alertstr.length; i++) {
+                        text += (i + 1) + "." + alertstr[i] + "\n";
                     }
-                    if (returnval == 1) {
-                        var text = "";
-                        var i;
-                        for (i = 0; i < alertstr.length; i++) {
-                            text += (i + 1) + "." + alertstr[i] + "\n";
-                        }
-                        alert(text);
-                        return false;
-                    }                
+                    alert(text);
+                    return false;
+                }
             } else {
                 alert('1.Please select Tax Type.\n2.Please select Commodity/Description. \n3.Please enter Vehicle Number.');
                 return false;
@@ -225,3 +225,113 @@
         });
     });
 </script>
+<script>
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            mapTypeControl: false,
+            center: {lat: 31.1048145, lng: 77.1734033},
+            zoom: 13
+        });
+
+        new AutocompleteDirectionsHandler(map);
+    }
+
+    /**
+     * @constructor
+     */
+    function AutocompleteDirectionsHandler(map) {
+        this.map = map;
+        this.originPlaceId = null;
+        this.destinationPlaceId = null;
+        this.travelMode = 'DRIVING';
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        this.directionsDisplay.setMap(map);
+
+        var originInput = document.getElementById('origin-input');
+        var destinationInput = document.getElementById('destination-input');
+        var modeSelector = document.getElementById('mode-selector');
+
+        var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+// Specify just the place data fields that you need.
+        originAutocomplete.setFields(['place_id']);
+
+        var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+// Specify just the place data fields that you need.
+        destinationAutocomplete.setFields(['place_id']);       
+        this.setupClickListener('changemode-transit', 'TRANSIT');
+        this.setupClickListener('changemode-driving', 'DRIVING');
+
+        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+    }
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+    AutocompleteDirectionsHandler.prototype.setupClickListener = function (
+            id, mode) {
+        var radioButton = document.getElementById(id);
+        var me = this;
+
+        radioButton.addEventListener('click', function () {
+            me.travelMode = mode;
+            me.route();
+        });
+    };
+
+    AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function (
+            autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+
+        autocomplete.addListener('place_changed', function () {
+            var place = autocomplete.getPlace();
+
+            if (!place.place_id) {
+                window.alert('Please select an option from the dropdown list.');
+                return;
+            }
+            if (mode === 'ORIG') {
+                me.originPlaceId = place.place_id;
+            } else {
+                me.destinationPlaceId = place.place_id;
+            }
+            me.route();
+        });
+    };
+
+    AutocompleteDirectionsHandler.prototype.route = function () {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+            return;
+        }
+        var me = this;
+
+        this.directionsService.route(
+                {
+                    origin: {'placeId': this.originPlaceId},
+                    destination: {'placeId': this.destinationPlaceId},
+                    travelMode: this.travelMode
+                },
+                function (response, status) {
+                    if (status === 'OK') {
+                        me.directionsDisplay.setDirections(response);
+                    } else {
+                        window.alert('Directions request failed due to ' + status);
+                    }
+                });
+    };
+
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBYb8Qt5VL-SALXmCeycEkaNtNypMuDuE&libraries=places&callback=initMap" async defer></script>
+<!--    <script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBYb8Qt5VL-SALXmCeycEkaNtNypMuDuE&callback=initMap">
+</script>-->
