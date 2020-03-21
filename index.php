a18f180d91dbaef61f9c4d3a00aa87e3cf4cd3ca
vasimlook <vasimlook@gmail.com>
2020-03-21 18:42:05 +0530

    http only
    http only

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-date_default_timezone_set('UTC');      
+date_default_timezone_set('UTC'); 
+$lifetime=600;
+session_set_cookie_params($lifetime);
 session_start();
+setcookie(session_name(),session_id(),time()+$lifetime,'/',null,null,TRUE);
 if(!isset($_SESSION['securityToken1'])){
     $_SESSION['securityToken1']=bin2hex(random_bytes(24));
 }
+//header('Set-Cookie:cross-site-cookie=bar; SameSite=None; Secure;HttpOnly');
 header("X-XSS-Protection: 1; mode=block");
 header('X-Content-Type-Options: nosniff');
 header('X-Frame-Options: deny');
 header('X-Powered-By:');
 //header('Set-Cookie: same-site-cookie=foo; SameSite=None');
-header('Set-Cookie:cross-site-cookie=bar; SameSite=None; Secure;HttpOnly');
+//header('Set-Cookie:cross-site-cookie=bar; SameSite=None; Secure;HttpOnly');
+//header('Set-Cookie: cross-site-cookie=bar; SameSite=None; Secure');
 //header('Set-Cookie:HttpOnly;Secure;SameSite=Strict');
 //response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
 
