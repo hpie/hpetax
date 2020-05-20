137e8c6e7963bde03b3a8badf49f1f6875946367
vasimlook <vasimlook@gmail.com>
2020-05-19 23:02:29 -0700

    https and http
    https and http

--- index.php
+++ index.php
@@ -18,11 +18,11 @@ header('X-Powered-By:');
 //header('Set-Cookie: cross-site-cookie=bar; SameSite=None; Secure');
 //header('Set-Cookie:HttpOnly;Secure;SameSite=Strict');
 //response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
-
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $r = $_SERVER['SCRIPT_NAME'];
 $subdomain = explode('/', $r);
 array_pop($subdomain);
-define('BASE_URL', 'https://'.$_SERVER['HTTP_HOST'].  implode('/', $subdomain).'/');
+define('BASE_URL', $protocol.$_SERVER['HTTP_HOST'].  implode('/', $subdomain).'/');
 define('BASE_URL_API', 'http://'.$_SERVER['HTTP_HOST'].  implode('/', $subdomain).'/');
 define('IMG_URL', BASE_URL.'uploads/');
 define('IMG_DIR', 'uploads/');
